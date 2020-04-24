2e8ef6a56ea219d106fbe37edbca6d041277b1e0
Suhaib <suhaib92khan@gmail.com>
2020-04-23 20:55:42 -0400

    Add postgraphile middleware

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const { Pool, Client } = require('pg');
+const { postgraphile } = require("postgraphile");
+// const { Pool, Client } = require('pg');
 const bodyParser = require("body-parser");
-const user = require('./db/user');
+const keys = require("../config/keys");
+const db = keys.connectionString;
+// const user = require('./db/user');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', user.getUsers);
+app.use(
+  postgraphile(
+    process.env.connectionString || db,
+    "public",
+    {
+      watchPg: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+    }
+  )
+);
+
+// app.get('/', user.getUsers);
 
 module.exports = app;
 
