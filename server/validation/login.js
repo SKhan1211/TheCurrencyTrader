4d3e7fe519962f7baa5dbab7835fb97285b34acf
Suhaib <suhaib92khan@gmail.com>
2020-04-24 20:01:08 -0400

    Add login validation

---
+++ server/validation/login.js
@@ -0,0 +1,20 @@
+const Validator = require("validator");
+const validText = require("./valid-text");
+
+module.exports = function validateLoginInput(data) {
+  data.username = validText(data.username) ? data.username : "";
+  data.password = validText(data.password) ? data.password : "";
+
+  if (Validator.isEmpty(data.username)) {
+    return { message: "Email is required", isValid: false };
+  };
+
+  if (Validator.isEmpty(data.password)) {
+    return { message: "Password is required", isValid: false };
+  };
+
+  return {
+    message: "",
+    isValid: true
+  };
+};
