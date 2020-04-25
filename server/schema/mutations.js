33b915aea7e44210af7506df220d1bad4262c265
Suhaib <suhaib92khan@gmail.com>
2020-04-25 12:53:05 -0400

    Add register mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,9 +1,25 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
 
+const AuthService = require("../services/auth");
+
 const mutation = new GraphQLObjectType({
   name: "Mutation",
-  fields: {}
+  fields: {
+    register: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        username: { type: GraphQLString },
+        password: { type: GraphQLString },
+        first_name: { type: GraphQLString },
+        last_name: { type: GraphQLString }
+      },
+      resolve(_, args) {
+        return AuthService.register(args);
+      }
+    }
+  }
 });
 
 module.exports = mutation;
