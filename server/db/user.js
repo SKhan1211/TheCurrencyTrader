5dc3dd7123a8e717ab613b540608969e56aefcd2
Suhaib <suhaib92khan@gmail.com>
2020-04-24 23:57:20 -0400

    Add saveUser PostgreSQL query

--- server/db/user.js
+++ server/db/user.js
@@ -1,7 +1,7 @@
 const db = require("./query");
 
 const User = {
-  async getUsers(req, res) {
+  async getUsers() {
     try {
       const readAllQuery = 'SELECT * FROM users';
       const { rows } = await db.query(readAllQuery);
@@ -10,11 +10,24 @@ const User = {
       return error;
     };
   },
-  async getUser(req, res) {
+  async getUser(email) {
     try {
       const query = {
         text: 'SELECT * FROM users WHERE email = $1',
-        values: [req.data.email],
+        values: [email],
+      }
+      const { rows } = await db.query(query);
+      return rows;
+    } catch (error) {
+      return error;
+    };
+  },
+  async saveUser(userInput) {
+    try {
+      const { email, username, password, first_name, last_name } = userInput;
+      const query = {
+        text: 'INSERT INTO users(email, username, password, first_name, last_name) VALUES($1, $2, $3, $4, $5',
+        values: [email, username, password, first_name, last_name],
       }
       const { rows } = await db.query(query);
       return rows;
--- server/services/auth.js
+++ server/services/auth.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-// const User = 
+const User = require("../db/user");
 const keys = require("../../config/keys");
 
 const validateRegisterInput = require("../validation/register");
@@ -15,7 +15,26 @@ const register = async data => {
 
     const { email, username, password, first_name, last_name } = data;
     
-    
+    const existingUser = await User.getUser(email);
+
+    if (existingUser) {
+      throw new Error("Sorry, this user already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = User.saveUser(
+      {
+        email,
+        username,
+        password: hashedPassword,
+        first_name,
+        last_name
+      },
+      err => {
+        if (err) throw err;
+      }
+    )
   } catch (err) {
     throw err;
   };
