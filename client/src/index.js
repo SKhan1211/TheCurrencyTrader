be02cb13e569d9f09593f6f1a057f84a372ed086
Suhaib <suhaib92khan@gmail.com>
2020-05-02 15:51:32 -0400

    Instantiate cache, httpLink, and client

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
+const cache = new InMemoryCache({
+  dataIdFromObject: object => object.id || null
+});
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:5000/graphql"
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([errorLink, httpLink]),
+  cache,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log("graphQLErrors", graphQLErrors);
+    console.log("networkError", networkError);
+  };
+});
+
+
 
 ReactDOM.render(
   <React.StrictMode>
--- design_documents/tasks/todos.md
+++ design_documents/tasks/todos.md
@@ -2,3 +2,4 @@
 2. [ ] Rewrite tests with testing database
 3. [ ] Finish designing frontend layout on XD
 4. [ ] Continue frontend user authentication - Started 05/02/20 Completed (in progress)
+5. [ ] Find out if src/index.js line 18 uri key in httpLink object needs an environment variable
